890a0cf042b2d704a0b54f316f73524d087592ab
itoandedi <52193155+ito-ao@users.noreply.github.com>
2020-10-30 18:16:31 +0900

    Add files via upload

---
+++ Scripts/CardController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    CardView view; // 見かけ(view)に関することを操作
+    CardModel model; // データ(model)に関することを操作
+
+    private void Awake()
+    {
+        view = GetComponent<CardView>();
+    }
+
+    public void Init(int cardID)
+    {
+        model = new CardModel(cardID);
+        view.Show(model);
+    }
+}
---
+++ Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // 手札にカード生成
+    [SerializeField] Transform playerHandTransform;
+    [SerializeField] CardController cardPrefad;
+
+    bool isPlayerTurn;
+
+    void Start()
+    {
+        StratGame();
+        CreateCard(playerHandTransform);
+    }
+
+    void StartGame(){
+        SettingInitHand();
+        isPlayerTurn = true;
+        TurnCalc();
+    }
+
+    void CreateCard(Transform hand)
+    {
+        CardController card = Instantiate(cardPrefad,playerHandTransform,false);
+        card.Init(1);
+    }
+
+}
