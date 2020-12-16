cdac757aa04fbbc16e8b27c47ad5c3d196f2f33d
itoandedi <52193155+ito-ao@users.noreply.github.com>
2020-12-16 13:33:32 +0900

    ターンエンドの追加
    エンド処理とドローの処理の追加

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // 手札にカード生成
-    [SerializeField] Transform playerHandTransform;
+    [SerializeField] Transform playerHandTransform,
+    enemyHandTransform;
     [SerializeField] CardController cardPrefad;
 
     bool isPlayerTurn;
 
     void Start()
     {
-        StratGame();
-        CreateCard(playerHandTransform);
+    StartGame();
     }
 
     void StartGame(){
@@ -22,10 +22,55 @@ public class GameManager : MonoBehaviour
         TurnCalc();
     }
 
+    void SettingInitHand(){
+        for (int i = 0; i<3; i++)
+        {
+            CreateCard(playerHandTransform);
+            CreateCard(enemyHandTransform);
+
+        }
+    }
+
+    void TurnCalc()
+    {
+        if(isPlayerTurn)
+        {
+         PlayerTurn();
+        }else
+        {
+            EnemyTurn();
+        }
+    }
+
+    public void ChangeTurn()
+    {
+        isPlayerTurn = !isPlayerTurn;
+        if(isPlayerTurn)
+        {
+            CreateCard(playerHandTransform);
+        }else
+        {
+            CreateCard(enemyHandTransform);
+        }
+        TurnCalc();
+    }
+
+    void PlayerTurn()
+    {
+        Debug.Log("playerのターン");
+    }
+
+    void EnemyTurn()
+    {
+        Debug.Log("enemyのターン");
+        ChangeTurn();
+    }
+
+
     void CreateCard(Transform hand)
     {
-        CardController card = Instantiate(cardPrefad,playerHandTransform,false);
-        card.Init(1);
+        CardController card = Instantiate(cardPrefad,hand,false);
+        card.Init(2);
     }
 
 }
